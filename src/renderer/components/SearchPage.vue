4122383b61a292224c4eb48603730d0c5dea085f
Secr3ts <42939294+Secr3ts@users.noreply.github.com>
2019-12-05 21:24:59 +0100

    'Changed.'

--- src/renderer/components/SearchPage.vue
+++ src/renderer/components/SearchPage.vue
@@ -6,6 +6,7 @@
       <h4 class="center-align">Search on the widest Music Platform.</h4>
       <div class="section">
         <div class="divider" :style="{ 'background-color': scColor }"></div>
+        <carousel></carousel>
       </div>
     </div>
   </div>
@@ -14,13 +15,15 @@
 <script>
 import Header from './Header'
 import InputField from './SearchPage/InputField'
+import Carousel from './SearchPage/Carousel'
 
 export default {
   name: 'search-page',
   components: {
     'appHeader': Header,
-    'inputField': InputField
-  },
+    'inputField': InputField,
+    'carousel': Carousel
+},
   data () {
     return {
       scColor: this.$parent.scColor
---
+++ src/renderer/components/SearchPage/Carousel.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="wrapper">
+    <div class="container">
+      <div class="carousel-container">
+        <el-carousel :interval="3000" type="card" :height="{ coverSize }">
+          <el-carousel-item><img src="./assets/bones-noize_undead.jpg" alt="Bones Noize Undead Cover"></el-carousel-item>
+          <el-carousel-item><img src="" alt=""></el-carousel-item>
+          <el-carousel-item><img src="" alt=""></el-carousel-item>
+          <el-carousel-item><img src="" alt=""></el-carousel-item>
+          <el-carousel-item><img src="" alt=""></el-carousel-item>
+          <el-carousel-item><img src="" alt=""></el-carousel-item>
+        </el-carousel>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { Carousel, CarouselItem } from 'element-ui'
+
+export default {
+  name: 'carousel',
+  data() {
+    return {
+      coverSize: '500px',
+      covers: [
+        {
+          img: ''
+        }
+      ]
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
